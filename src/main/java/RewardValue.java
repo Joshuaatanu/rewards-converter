480bd4d2c51c924963edddaea61c79db57bc68b7
Joshua Atanu <atanu.joshua@gmail.com>
2023-04-09 19:34:03 +0100

    finished the code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,23 @@
 public class RewardValue {
+    private final double cashValue;
+    public static final double milesToCashConversionRate= 0.0035;
+    public RewardValue( double cashValue){
+        this.cashValue= cashValue;
+    }
+
+    public RewardValue(int milesValue){
+        this.cashValue = convertToMiles(milesValue);
+    }
+    public static int convertToMiles(double cashValue){
+        return (int) ((cashValue)/ milesToCashConversionRate);
+    } //convert to miles method
+    public static double convertToCash(int milesValue){
+        return milesValue * milesToCashConversionRate;
+    }
+    public  double getCashValue(){
+        return cashValue;
+    }
+    public int getMilesValue(){
+        return  convertToMiles(this.cashValue);
+    }
 }
